741d80f332d4133dd642664e0dd435395e56729e
Halil YILDIRIM <halil.yildirim@isinolacak.com>
2026-03-28 00:14:08 +0300

    feat(api): add app.module

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule, OrderModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
